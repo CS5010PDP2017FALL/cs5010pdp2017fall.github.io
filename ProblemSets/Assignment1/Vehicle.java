c190c176188328e011f8a6ad59063c7556a87c53
mrinalsourav87 <mrinalsourav87@ccs.neu.edu>
2017-09-09 21:30:00 -0700

    Posting Assignment1
    Posting Assignment1

---
+++ ProblemSets/Assignment1/Vehicle.java
@@ -0,0 +1,41 @@
+/**
+ * This is part of Assignment 1: Environment Setup and Review of Java for PDP, Fall 2017.
+ */
+package Assignment1;
+
+/**
+ * Vehicle is a simple object that has a velocity and a direction.
+ */
+// You may not make Ball implement the Comparable interface.
+public class Vehicle {
+
+   private double velocity;
+   private int direction;
+   
+    /**
+     * Constructor that creates a new vehicle object with the specified velocity and direction.
+     * @param velocity Velocity of the new object.
+     * @param direction Direction of the new object, where 1 represents eastbound direction, and 2 westbound direction.
+     */
+    public Vehicle(double velocity, int direction) {
+        velocity = velocity;
+        direction = int;
+    }
+
+    /**
+     * Returns the velocity of the Vehicle.
+     * @return the velocity of the Vehicle.
+     */
+    public double getVelocity() {
+        return 0;
+    }
+
+   /**
+     * Returns the direction of the Vehicle.
+     * @return the direction of the Vehicle.
+     */
+    public double getDirection() {
+        return 0;
+    }
+}
+
